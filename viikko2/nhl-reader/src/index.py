2ad85974b3b3be7ee475aaa429421053f5c97b48
Sofia Kettunen <sofia.kettunen@helsinki.fi>
2025-11-08 20:25:20 +0200

    viikko 2 tehtävä 5

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,18 +1,37 @@
 from playerreader import PlayerReader
 from playerstats import PlayerStats
+from rich import print
+from rich.prompt import Prompt
+from rich.table import Table
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    seasons = [f"20{i}-{i+1}" for i in range(18, 26)]
+
+    season = Prompt.ask("Season", choices=seasons, default="2024-25")
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
 
-    country = input("Print players from: ")
-    players = stats.top_scorers_by_nationality(country)
+    nationalities = stats.nationalities()
 
-    print(f"\nPlayers from {country}:")
+    while True:
+        nationality = Prompt.ask("Nationality", choices=nationalities)
+        players = stats.top_scorers_by_nationality(nationality)
 
-    for player in players:
-        print(player)
+        table = Table(title=f"Season {season} players from {nationality}")
+
+        table.add_column("name", style="cyan")
+        table.add_column("teams", style="magenta")
+        table.add_column("goals", justify="right", style="green")
+        table.add_column("assists", justify="right", style="green")
+        table.add_column("points", justify="right", style="yellow")
+        table.add_column("games", justify="right", style="red")
+
+        for player in players:
+            table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.points()), str(player.games))
+
+        print(table)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/playerstats.py
+++ viikko2/nhl-reader/src/playerstats.py
@@ -2,5 +2,8 @@ class PlayerStats():
     def __init__(self, reader):
         self.players = reader.get_players()
 
+    def nationalities(self):
+        return list(set(player.nationality for player in self.players))
+
     def top_scorers_by_nationality(self, country):
         return sorted((p for p in self.players if p.nationality == country), key=lambda p: p.points(), reverse=True)
